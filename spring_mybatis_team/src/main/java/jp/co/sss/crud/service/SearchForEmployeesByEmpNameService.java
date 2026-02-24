138296f73fcca9d00b3f22a5e8526f3ceec1c2fb
r-nakao <nakaoryuto.tis2025@gmail.com>
2026-02-24 15:15:37 +0900

    fix:中尾-WBS09社員名検索

--- spring_mybatis_team/src/main/java/jp/co/sss/crud/service/SearchForEmployeesByEmpNameService.java
+++ spring_mybatis_team/src/main/java/jp/co/sss/crud/service/SearchForEmployeesByEmpNameService.java
@@ -16,9 +16,21 @@ public class SearchForEmployeesByEmpNameService {
 	/**
 	 * 社員名検索表示
 	 * 
+	 * 条件指定なし（null/空文字）で検索実行した場合は全件表示
+	 * 入力チェックは行わない
+	 * 
+	 * @param empName 社員名
 	 * @return 社員エンティティ
+	 * author 中尾
 	 */
 	public List<Employee> execute(String empName) {
+
+		// 条件指定せずに検索実行した場合は、社員情報の全件一覧を表示
+		if (empName == null || empName.isEmpty()) {
+			return mapper.findAllEmployeesOrderByEmpId();
+		}
+
+		// LIKE検索（部分一致）		
 		return mapper.findByEmpNameContainingOrderByEmpId(empName);
 	}
 
